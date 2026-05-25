27b5ceb33276d63dd7b5158019c376696e8f3e0d
b22pchg-arch <b22pchg@gmail.com>
2026-05-25 17:05:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'planflow-v5-cache';
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['PlanFlow_v5.html'])));
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
 });
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
